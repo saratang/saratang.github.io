1711e121fda3bb820b28033759c6cf88f9d992ea
Sara Tang <sara_z_tang@yahoo.ca>
2014-12-24 09:02:30 +0800

    Update CharitiesNearMe

--- CharitiesNearMe/main_functions.js
+++ CharitiesNearMe/main_functions.js
@@ -1,53 +1,64 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see a blank space instead of the map, this
-// is probably because you have denied permission for location sharing.
-
-var map;
-
 function initialize() {
-var mapOptions = {
-  zoom: 6
-};
-map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+    var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+  
+// // Get current location
+// // Note: This example requires that you consent to location sharing when
+// // prompted by your browser. If you see a blank space instead of the map, this
+// // is probably because you have denied permission for location sharing.
 
-// Try HTML5 geolocation
-if(navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = new google.maps.LatLng(position.coords.latitude,
-                                     position.coords.longitude);
+// var map;
 
-    var infowindow = new google.maps.InfoWindow({
-      map: map,
-      position: pos,
-      content: 'Location found using HTML5.'
-    });
+// function initialize() {
+// var mapOptions = {
+//   zoom: 6
+// };
+// map = new google.maps.Map(document.getElementById('map-canvas'),
+//     mapOptions);
 
-    map.setCenter(pos);
-  }, function() {
-    handleNoGeolocation(true);
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleNoGeolocation(false);
-}
-}
+// // Try HTML5 geolocation
+// if(navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     var pos = new google.maps.LatLng(position.coords.latitude,
+//                                      position.coords.longitude);
 
-function handleNoGeolocation(errorFlag) {
-if (errorFlag) {
-  var content = 'Error: The Geolocation service failed.';
-} else {
-  var content = 'Error: Your browser doesn\'t support geolocation.';
-}
+//     var infowindow = new google.maps.InfoWindow({
+//       map: map,
+//       position: pos,
+//       content: 'Location found using HTML5.'
+//     });
 
-var options = {
-  map: map,
-  position: new google.maps.LatLng(60, 105),
-  content: content
-};
+//     map.setCenter(pos);
+//   }, function() {
+//     handleNoGeolocation(true);
+//   });
+// } else {
+//   // Browser doesn't support Geolocation
+//   handleNoGeolocation(false);
+// }
+// }
 
-var infowindow = new google.maps.InfoWindow(options);
-map.setCenter(options.position);
-}
+// function handleNoGeolocation(errorFlag) {
+// if (errorFlag) {
+//   var content = 'Error: The Geolocation service failed.';
+// } else {
+//   var content = 'Error: Your browser doesn\'t support geolocation.';
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// var options = {
+//   map: map,
+//   position: new google.maps.LatLng(60, 105),
+//   content: content
+// };
+
+// var infowindow = new google.maps.InfoWindow(options);
+// map.setCenter(options.position);
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
