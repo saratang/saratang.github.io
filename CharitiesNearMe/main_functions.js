0ee250555fe48d22583db4d665b0ce9afcecbf60
Sara Tang <sara_z_tang@yahoo.ca>
2014-12-27 08:49:03 +0800

    Update main_functions.js

--- CharitiesNearMe/main_functions.js
+++ CharitiesNearMe/main_functions.js
@@ -59,14 +59,52 @@ function initialize() {
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
-  }
+}
 
 function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
       'callback=initialize';
-  document.body.appendChild(script);
+    document.body.appendChild(script);
 }
 
 window.onload = loadScript;
+
+//Find all places within a limit
+function ajax_request(token, start, end, location) {
+    return $.ajax({
+        url: "https://www.eventbriteapi.com/v3/events/search?token="+ token + "&categories=111&start_date.  range_start=" + start+ "&start_date.range_end=" + end + "&venue.city=" + location,
+        type: "GET",
+        dataType: "json",
+        cache: "false"
+    });
+}
+
+function thirty_days_later() {
+    var thirty_days_later = new Date();
+    thirty_days_later.setDate(thirty_days_later.getDate() + 30);
+    thirty_days_later.toUTCString().slice(0, 10);
+    return thirty_days_later;
+}
+
+$(function() {
+    $('#refine').submit(function() {
+        var location = 'toronto';
+        var token = "ZBEVEGMUTNYPFPOKE4B7";
+        var today = new Date().toUTCString().slice(0, 10);
+        var thirty_days_later = thirty_days_later();
+        var params = {'hostname': $hostname, 'type': $type};
+        var res = ajax_request(token, today, thirty_days_later, location);
+        res
+            .done(function(data) {
+                if (data && !data.error) {
+                    $('#target').html(data.events);
+                } else {
+                    $('#target').html('Error...');
+                }
+          //if data is non-empty, return an array containing data['events']['name']['text'], data['events']['description']['text'], data['events']['url'], data['events']['start']['local'], host name, event address, and number of people attending
+          //if data is empty, display some message
+            });
+    });
+});
